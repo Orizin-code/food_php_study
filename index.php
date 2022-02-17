188aa871161ca8e11cff5dd54e401f33a482948d
Orizin <nlgporizin10@gmail.com>
2022-02-18 00:15:52 +0900

    ファイルの振り分け実装

--- index.php
+++ index.php
@@ -1,43 +1,21 @@
-<?php
-class Menu {
-  public $name;
-  
-  public function __construct($name) {
-    $this->name = $name;
-  }
-  
-  public function hello() {
-    echo '私は'.$this->name.'です';
-  }
-}
-
-$juice = new Menu('JUICE');
-$coffee = new Menu('COFFEE');
-$curry = new Menu('CURRY');
-$pasta = new Menu('PASTA');
-
-$menus = array($juice,$coffee,$curry,$pasta);
-
-?>
+<?php require_once('data.php'); ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <title>Café App</title>
-  <link rel="stylesheet" type="text/css" href="stylesheet.css">
-  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
-</head>
-<body>
-  <div class="menu-wrapper container">
-    <h1 class="logo">Café App</h1>
-    <div class="menu-items">
-
-      <?php foreach ($menus as $menu):?>
-        <h3><?php echo $menu->name ?></h3>
-      <?php endforeach ?>
-      
+  <head>
+    <meta charset="utf-8">
+    <title>Café App</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div class="menu-wrapper container">
+      <h1 class="logo">Café App</h1>
+      <div class="menu-items">
+        <?php foreach ($menus as $menu): ?>
+          <h3 class="menu-item-name"><?php echo $menu->name ?></h3>
+        <?php endforeach ?>      
+      </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
