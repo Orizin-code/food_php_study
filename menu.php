ef30bc5e1335f1f756eb8cca5fbb14f401bc84e0
Orizin <nlgporizin10@gmail.com>
2022-02-20 04:50:47 +0900

    税込価格の表示設定

--- menu.php
+++ menu.php
@@ -2,11 +2,14 @@
   class Menu {
     public $name;
     public $price;
-    public $image;
     
     public function __construct($name, $price) {
       $this->name = $name;
       $this->price = $price;
+    }
+
+    public function getTaxIncludedPrice() {
+      return floor($this->price * 1.08);
     }
   }
 ?>
